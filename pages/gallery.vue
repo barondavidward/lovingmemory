bb9abf7240c2363a8ccb0169105117f068339d5b
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-05-14 22:46:19 +0100

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -4,7 +4,8 @@
       <Nav />
     </div>
     <div class="section details">
-      <h1 class="title">Gallery</h1>
+      <h1 class="title">Tributes</h1>
+      <p>If you would like to share your tribute to Baron David Ward, please send audio snippets, brief videos, comments, experiences, pictures, poems, short stories or memoirs to <a href=mailto:tributes@barondavidward.com> tributes@barondavidward.com</a>
       <CoolLightBox 
         :items="images" 
         :index="index"
