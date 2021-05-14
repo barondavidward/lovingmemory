ce914fb230e5c220aa73a701fb98f3296de0c21b
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-05-14 16:13:15 +0100

    changes

---
+++ index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="section links">
+      <Nav />
+    </div>
+    <div class="section tribute">
+      <h1 class="title">In Loving Memory</h1>
+      <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
+      <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+
+</style>
