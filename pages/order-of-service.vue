ae34c9e3e8f0ab3ebe0c5ab7ac48e0abdb607a10
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-06-01 14:10:53 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -6,15 +6,6 @@
     <div class="section details">
       <h1 class="title">Order of Service</h1>
       <p class="highlight"><em>Date TBC</em> Details will appear here in due course.</p>
-<html>
-  <head>
-    <title>Title of the document</title>
-  </head>
-  <body>
-    <iframe src="http://www.dropbox.com/s/2metx0xttgfu0u2/draft.pdf?dl=0#toolbar=0" width="100%" height="500px">
-    </iframe>
-  </body>
-</html>
 
     </div>
   </div>
