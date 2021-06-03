d3e6a7e9a05c8df2f5f2b1f5c1293d64b2d2421d
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-06-04 00:52:51 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -23,7 +23,7 @@
         <ListItem>Montage and Reflection</ListItem>
         <ListItem>Closing Remarks and Committal</ListItem>
         <ListItem>Committal Music: TBC</ListItem>
-	<ListItem>Committal Music: TBC</ListItem>
+	<ListItem>Exit Music: Dedication by David's Son and Daughter</ListItem>
 	<ListItem>Planting a Cherry Tree in accordance with David's wishes</ListItem>
 	<ListItem>Celebration of Light and Music dedications follow this service.</ListItem>
       </ul>
