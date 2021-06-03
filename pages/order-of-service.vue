393edd547c3f0772e2ab383770d777e3106096e1
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-06-03 16:47:39 +0100

    update order of service

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,8 +5,28 @@
     </div>
     <div class="section details">
       <h1 class="title">Order of Service</h1>
-      <p class="highlight"><em>Date TBC</em> Details will appear here in due course.</p>
-
+      <p><em>Saturday 5th June 2021</em> Due to COVID-19 the service is restricted in the number of people allowed. The plan is to live-stream the service so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of light and music offerings after the service.</p>
+      <p class="highlight">The family and friends are requesting Donations in lieu of flowers for The Natural Death Centre please visit www.justgiving.com/crowdfunding/barondavid thank you.</p>
+      <h2>The Service <span>TIME TBCpm</span></h2>
+      <ul class="fa-ul">
+        <ListItem>Music Before Service: TBC</ListItem>
+        <ListItem>Music Before Service: TBC</ListItem>
+        <ListItem>Service starts at TBC </ListItem>
+        <ListItem>Entrance music: TBC</ListItem>
+	<ListItem>Entrance music: TBC</ListItem>
+        <ListItem>Welcome and Opening Address</ListItem>
+        <ListItem>Music: Amazing Grace (My Chains Are Gone)</ListItem>
+        <ListItem>Tributes & Memories</ListItem>
+        <ListItem>A Poem read by his sister Susan</ListItem>
+        <ListItem>Friend Tribute: David</ListItem>
+        <ListItem>Eulogy by Mike</ListItem>
+        <ListItem>Montage and Reflection</ListItem>
+        <ListItem>Closing Remarks and Committal</ListItem>
+        <ListItem>Committal Music: TBC</ListItem>
+	<ListItem>Committal Muisc: TBC</ListItem>
+	<ListItem>Planting a Cherry Tree in accordance with David's wishes</ListItem>
+	<ListItem>Celebration of Light and Music dedications follow this service.</ListItem>
+      </ul>
     </div>
   </div>
 </template>
