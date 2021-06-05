cb718426adcfdabc5cff0fb4a90fab919b6683c1
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-06-05 09:03:31 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -7,7 +7,7 @@
       <h1 class="title">Order of Service</h1>
       <p><em>Saturday 5th June 2021</em> Due to COVID-19 the service is restricted in the number of people allowed. The plan is to live-stream the service so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of light and music offerings after the service.</p>
       <p class="highlight">The family and friends wish to express their appreciation for the kindness, sympathy, and support which has been extended to them in their bereavement.</p>
-      <h2>The Service <span>14:00 (British Summer Time)</span></h2>
+      <h2>The Service <span>14:00 (BST)</span></h2>
       <ul class="fa-ul">
         <ListItem>Music Before Service: TBC</ListItem>
         <ListItem>Music Before Service: TBC</ListItem>
