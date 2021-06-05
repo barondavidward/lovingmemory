5963189b2c5572e742c9e6f14fda6f40e919bf99
barondavidward <84179337+barondavidward@users.noreply.github.com>
2021-06-05 09:51:32 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -9,11 +9,11 @@
       <p class="highlight">The family and friends wish to express their appreciation for the kindness, sympathy, and support which has been extended to them in their bereavement.</p>
       <h2>The Service <span>14:00 (BST)</span></h2>
       <ul class="fa-ul">
-        <ListItem>Music Before Service: TBC</ListItem>
-        <ListItem>Music Before Service: TBC</ListItem>
-        <ListItem>Service starts at TBC </ListItem>
-        <ListItem>Entrance music: TBC</ListItem>
-	<ListItem>Entrance music: TBC</ListItem>
+        <ListItem>Music Before Service: Tina Turner - We Don’t Need Another Hero (Thunderdrome)</ListItem>
+        <ListItem>Music Before Service: Eminem – I’m Not Afraid</ListItem>
+        <ListItem>Service starts at 14:00 </ListItem>
+        <ListItem>Entrance music: Duff Kelly – Holy Lady</ListItem>
+	<ListItem>Entrance music: Nahko and Medicine For The People – Love Letters To God</ListItem>
         <ListItem>Welcome and Opening Address</ListItem>
         <ListItem>Music: Amazing Grace (My Chains Are Gone)</ListItem>
         <ListItem>Tributes & Memories</ListItem>
@@ -22,10 +22,10 @@
         <ListItem>Eulogy by Mike</ListItem>
         <ListItem>Montage and Reflection</ListItem>
         <ListItem>Closing Remarks and Committal</ListItem>
-        <ListItem>Committal Music: TBC</ListItem>
-	<ListItem>Exit Music: Dedication by David's Son and Daughter</ListItem>
+        <ListItem>Committal Music: Xavier Rudd - Spirit Bird</ListItem>
+	<ListItem>Exit Music: Dedication by David's Son and Daughter (Chris De Burgh - Don't Pay The Ferryman)</ListItem>
 	<ListItem>Planting a Cherry Tree in accordance with David's wishes</ListItem>
-	<ListItem>Music whilst planting tree: TBC</ListItem>
+	<ListItem>Music whilst planting tree: Nahko and Medicine For The People - Dear Brother</ListItem>
 	<ListItem>Celebration of Light and Music dedications follow this service.</ListItem>
       </ul>
         <a class="button" target="_blank" rel=”noopener” href="https://www.dropbox.com/s/gas2rcq3tiq2j1p/Order%20of%20Service%20-%20Final.pdf?dl=0">Download Order of Service PDF</a>
